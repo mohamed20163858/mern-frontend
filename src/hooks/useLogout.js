1810af38303fe121b17b8a29ec59904625d266fb
mohamed saleh <mohamed20163858@gmail.com>
2024-01-12 16:05:45 +0200

    Fix memorization by set workouts to null when logout

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,9 +1,12 @@
 import { useAuthContext } from "./useAuthContext"
+import { useWorkoutsContext } from "./useWorkoutsContext";
 
 const useLogout = () => { 
     const { dispatch } = useAuthContext();
+    const { dispatch: workoutsDispatch } = useWorkoutsContext();
     const logout = () => {
         dispatch({type: "LOGOUT"});
+        workoutsDispatch({type: "SET_WORKOUTS", payload: null})
         localStorage.removeItem("user");
     }
    return { logout }
